2b4f67101f421ada623e8173e86efaf9d2d83a5e
shawnsandy04 <shawnsandy04@gmail.com>
2017-04-09 08:47:22 -0400

    update

--- src/resources/views/components/forms/edit.blade.php
+++ src/resources/views/components/forms/edit.blade.php
@@ -1,6 +1,5 @@
-{{ Form::model($data,  array_merge(["method" => "put", "url" => $url] )) }}
+{{ Form::model($data,  array_merge(["method" => "put", "url" => $url], $options )) }}
 
 {{ Form::dashFields($model) }}
-
 <hr>
 {{ Form::dashClose() }}
