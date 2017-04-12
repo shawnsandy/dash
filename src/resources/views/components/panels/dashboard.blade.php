807bce2c38caa35d41996097473786e59d5ada34
shawn sandy <shawnsandy04@gmail.com>
2017-04-12 13:57:48 -0400

    Add a conditional for icon sets
    Updates composer

--- src/resources/views/components/panels/dashboard.blade.php
+++ src/resources/views/components/panels/dashboard.blade.php
@@ -1,10 +1,8 @@
-@php
-if(!isset($icon)) $icon = "circle-o";
-@endphp
 <div class="panel panel-default component-panel">
     <div class="panel-body">
         <h2 class="component-panel-title">
-            {{ Html::dashIcons($icon) }} {{ $title or 'Admin Panel' }}
+            {{ Html::dashIcons((isset($icon)) ?  $icon : "circle-o") }}
+            {{ $title or 'Admin Panel' }}
         </h2>
         <hr>
        {{ $slot }}
--- src/resources/views/components/panels/info.blade.php
+++ src/resources/views/components/panels/info.blade.php
@@ -1,6 +1,7 @@
 <aside class="panel">
     <div class="panel-body">
-        <p class="h4 text-uppercase">{{ Html::dashIcons() }}  {{ $title or "..." }}</p>
+        <p class="h4 text-uppercase">
+        {{ Html::dashIcons((isset($icon)) ?  $icon : "circle-o") }} {{ $title or "..." }}</p>
         <hr>
          {{ $slot }}
     </div>
