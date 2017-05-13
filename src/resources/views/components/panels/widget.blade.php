cd562ab9bd02cff5da226e88b2931315638a16ff
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-13 08:48:38 -0400

    wip

--- src/resources/views/components/panels/widget.blade.php
+++ src/resources/views/components/panels/widget.blade.php
@@ -1,13 +1,9 @@
 <aside class="panel {{ $class or "panel-component" }}">
 
-
-
-
     <div class="panel-body">
         <h4>{{ Html::dashIcons((isset($icon)) ?  $icon : "circle-o") }} {{ $title }}</h4>
         <hr>
         {{ $slot }}
-
     </div>
 
 </aside>
