9debbbfa511ebc02674fca8a95eaa7669f6ef546
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-13 08:48:07 -0400

    remove panel heading add title to body

--- src/resources/views/components/panels/widget.blade.php
+++ src/resources/views/components/panels/widget.blade.php
@@ -1,11 +1,11 @@
 <aside class="panel {{ $class or "panel-component" }}">
 
-    <div class="panel-heading">
-        <h4>{{ Html::dashIcons((isset($icon)) ?  $icon : "circle-o") }} {{ $title }}</h4>
-    </div>
+
+
 
     <div class="panel-body">
-
+        <h4>{{ Html::dashIcons((isset($icon)) ?  $icon : "circle-o") }} {{ $title }}</h4>
+        <hr>
         {{ $slot }}
 
     </div>
