1f53bb6df4c465664a41fe89f475240923ca41a7
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-13 08:46:27 -0400

    remove hr below widget title

--- src/resources/views/components/panels/widget.blade.php
+++ src/resources/views/components/panels/widget.blade.php
@@ -2,7 +2,6 @@
 
     <div class="panel-heading">
         <h4>{{ Html::dashIcons((isset($icon)) ?  $icon : "circle-o") }} {{ $title }}</h4>
-        <hr>
     </div>
 
     <div class="panel-body">
