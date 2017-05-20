cc2f58d88808f91a344f42182f923dd5d5200f2a
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-20 16:32:32 -0400

    Touching file, close form component

--- src/resources/views/components/forms/dash-file.blade.php
+++ src/resources/views/components/forms/dash-file.blade.php
@@ -1,9 +1,9 @@
 <div class="form-group">
     <div class="clearfix">
         <label for="{{ $name }}">{{ $title }}</label>
-        <div class="{{ $name }} file-input-box" style="">
+        <div class="{{ $attributes["data-table"] or '' }} {{ $name }} file-input-box" style="">
 
-            <p class="file-label small" style="">UPLOAD FILE : <span class="file-upload-name"></span></p>
+            <p class="file-label small" style="">Upload {{ $name }} : <span class="file-upload-name"></span></p>
             <input type="file" name="{{ $name }}" id="{{ $name }}" class="file-upload" style=""/>
         </div>
         {!! Form::formError($name, $errors) !!}
@@ -25,7 +25,6 @@
         height: 100%;
         height: inherit;
         margin: 0;
-        padding: 10px;
     }
 
     .file-input-box [type=file] {
@@ -35,7 +34,6 @@
         position: absolute;
         top: 0;
         bottom: 0;
-        padding: 10px;
 
     }
 </style>
