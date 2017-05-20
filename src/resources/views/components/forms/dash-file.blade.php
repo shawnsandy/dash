5d265b25e1baf4c0fe09c79629510c93549a0015
shawnsandy04 <shawnsandy04@gmail.com>
2017-05-20 08:48:54 -0400

    Modify the file upload
    - add the missing nethor params
    - add style to upload component
    - add javascript function to detect file changes and display selected file name

--- src/resources/views/components/forms/dash-file.blade.php
+++ src/resources/views/components/forms/dash-file.blade.php
@@ -1,5 +1,55 @@
-<div class="form-group {{ $name }} file-input-box">
-    {!! Form::formLabel($title)  !!}
-    {{ Form::file($name) }}
+<div class="row">
+    <div class="form-group {{ $name }} file-input-box" style="">
+        <p class="file-label text-center" style="">{{ $title }} <span class="file-upload-name"></span></p>
+        <input type="file" name="{{ $name }}" id="{{ $name }}" class="file-upload" style=""/>
+    </div>
     {!! Form::formError($name, $errors) !!}
+
 </div>
+@push('styles')
+<style>
+    .file-input-box {
+        position: relative;
+        font-size: 18px;
+        width: 100%;
+        min-height: 40px;
+        padding: 10px
+
+    }
+
+    .file-label {
+        display: block;
+        height: 100%;
+        height: inherit;
+        margin: 0;
+        padding: 10px;
+    }
+
+    .file-input-box [type=file] {
+        min-height: 30px;
+        float: left;
+        opacity: 0;
+        position: absolute;
+        top: 0;
+        padding: 10px;
+
+    }
+</style>
+@endpush
+@push('scripts')
+<script>
+    $(document).ready(function() {
+        $(".file-upload").each(function(){
+            var file = $(this);
+
+            $(file).change(function(e) {
+                var upload_file = $(file).val();
+                var file_name = upload_file.replace("C:\\fakepath\\", '');
+                console.log("changed" + ' ' + upload_file + ' '+ file_name);
+                $(".file-upload-name").html(file_name);
+            });
+
+        });
+    });
+</script>
+@endpush
