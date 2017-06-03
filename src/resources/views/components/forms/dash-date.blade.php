08f594c064e0523d4bf7dd5fc60ede55395ef410
shawnsandy04 <shawnsandy04@gmail.com>
2017-06-03 13:19:23 -0400

    Feature - add rquired attribute to fields based on config setting

--- src/resources/views/components/forms/dash-date.blade.php
+++ src/resources/views/components/forms/dash-date.blade.php
@@ -1,6 +1,9 @@
-<div class="form-group {{ $name }} {{ $errors->first($name, "has-error") }}">
+@php
+    $required =  (config("forms.".$attributes["data-table"].".required.".$name, null ) ) ? "required" : null ;
+@endphp
+<div class="form-group {{ $required }} {{ $name }} {{ $errors->first($name, "has-error") }}">
     {!! Form::formLabel($title)  !!}
-    {{ Form::text($name, $value, array_merge(['class' => "form-control dash-date $name"], $attributes)) }}
+    {{ Form::text($name, $value, array_merge(['class' => "form-control dash-date $name", $required], $attributes)) }}
     {!! $errors->first($name, '<p class="text-danger">:message</p>') !!}
 </div>
 
--- src/resources/views/components/forms/dash-text.blade.php
+++ src/resources/views/components/forms/dash-text.blade.php
@@ -1,5 +1,8 @@
-<div class="form-group {{ $name }} {{ $errors->first($name, "has-error") }}">
+@php
+    $required =  (config("forms.".$attributes["data-table"].".required.".$name, null ) ) ? "required" : null ;
+@endphp
+<div class="form-group {{ $name }} {{ $errors->first($name, "has-error") }} {{ $required }}">
     {!! Form::formLabel($title)  !!}
-    {{ Form::text($name, $value, array_merge(['class' => "form-control dash-text {$attributes["data-table"]} {$name}"], $attributes)) }}
+    {{ Form::text($name, $value, array_merge(['class' => "form-control dash-text {$attributes["data-table"]} {$name}", $required ? "required" : null ], $attributes)) }}
     {!! $errors->first($name, '<p class="text-danger">:message</p>') !!}
 </div>
