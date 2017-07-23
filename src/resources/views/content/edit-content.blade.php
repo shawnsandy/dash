849010d9193adc14143d6ad5cd10c2bca658f6c0
shawnsandy04 <shawnsandy04@gmail.com>
2017-07-23 11:59:15 -0400

    Feature - image preview

--- src/resources/views/content/edit-content.blade.php
+++ src/resources/views/content/edit-content.blade.php
@@ -14,7 +14,7 @@
                         <div class="col-lg-12">
                             @component("dashelements::components.panel", ["heading" => 'h2'])
                                 @slot("title")
-                                    Edit Post
+                                    Editing <small>({{ $content->title }})</small>
                                 @endslot
                                 {{ Form::model($content, ["url" => "/bluelines/posts/$content->id", "method" => "put", "files" => true]) }}
 
@@ -35,9 +35,17 @@
             <!--  side bar -->
 
             <div class="col-md-3">
+
+                <aside class="panel">
+
+                    <img src="<?= $content->post_thumbnail ?>?w=400" class="img-responsive">
+
+                </aside>
+
                 <aside class="panel">
                     @include("bluelines::components.recent-content", ["post_url" => "/admin/content/"])
                 </aside>
+
                 <aside class="panel">
                     <div class="panel-body">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, atque commodi
