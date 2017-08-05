b4ddce91ae4df8eceb92420abfb19f375aeed726
shawnsandy04 <shawnsandy04@gmail.com>
2017-08-04 20:32:44 -0400

    Updates - add recent content to to views and remove demo widget

--- src/resources/views/content/create-content.blade.php
+++ src/resources/views/content/create-content.blade.php
@@ -29,17 +29,9 @@
             <!--  side bar -->
 
             <div class="col-md-3">
+
                 <aside class="panel">
-                    <div class="panel-heading">
-                        <p class="lead">Sidebar...</p>
-                    </div>
-                    <ul class="list-group">
-                        <li class="list-group-item">Cras justo odio</li>
-                        <li class="list-group-item">Dapibus ac facilisis in</li>
-                        <li class="list-group-item">Morbi leo risus</li>
-                        <li class="list-group-item">Porta ac consectetur ac</li>
-                        <li class="list-group-item">Vestibulum at eros</li>
-                    </ul>
+                    @include("bluelines::components.recent-content", ["post_url" => "/admin/content/"])
                 </aside>
 
                 <aside class="panel">
